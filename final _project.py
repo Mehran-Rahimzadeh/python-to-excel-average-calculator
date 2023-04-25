c06af0288c0974c9b343adce962291cbd6b4fc5b
Mehran-Rahimzadeh <mehran1988r@gmail.com>
2023-04-25 10:09:33 +0330

    geting a student name cours mark and credit and sve ina dictionary

--- final _project.py
+++ final _project.py
@@ -7,40 +7,98 @@ clear()
 # importing pyhton list to exell file module
 import xlsxwriter
 
-# creat empty list for student names and ther cours 
-firstNames=[]
-lastNames=[]
-coursNames=[]
-credits=[]
+# creat empty dics for student names and ther cours 
+firstStudent= dict(firstName="" , lastName = "", courses=[], credits=[], marks=[])
 
 
+# asking funxtion
 def ask():
-    fN= input('Please enter the student first name: ')
-    firstNames.append(fN)
-    lN= input('Please enter student last name: ')
-    lastNames.append(lN)
-    next_step()
     
+    print(firstStudent)
+    name_ask()
+    courses_ask()
+    next_step_student()
 
 
-# input method
-def next_step():
+# getting first and last name
+def name_ask():
+    firstStudent["firstName"]=input("Enter the student name: ")
+    firstStudent["lastName"]=input ("Enter the student last name: ")
+
+# getting courses
+def courses_ask():
+    firstStudent["courses"].append(input ("Enter the student course: "))
+    firstStudent["credits"].append(int(input ("Enter the course credit: ")))
+    firstStudent["marks"].append(float(input ("Enter the course mark: ")))
+    next_step_courses()
+
+
+
+
+
+# input method (courses)
+def next_step_courses():
+    answer=ord(input('Is there any other courses? y/n: '))
+    if answer==121:
+        courses_ask()
+    else:
+        return
+    
+    # input method (courses)
+def next_step_student():
     answer=ord(input('Is there any other students? y/n: '))
     if answer==121:
         ask()
     else:
         return
+    
+
+
+
+ask()
+print(firstStudent)
 
 
 
 
 
-new_list_0 = [['first', 'second'], ['third', 'four'], [1, 2, 65, 8, 5, 6]]
-new_list = [['First Names', 'Last Names', "courses", "credits", "Marks"]]
 
-with xlsxwriter.Workbook('test.xlsx') as workbook:
-    worksheet = workbook.add_worksheet()
 
-    for row_num, data in enumerate(new_list):
-        worksheet.write_row(row_num, 0, data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# new_list = [['First Names', 'Last Names', "courses", "credits", "Marks"]]
+
+# with xlsxwriter.Workbook('test.xlsx') as workbook:
+#     worksheet = workbook.add_worksheet()
+
+#     for row_num, data in enumerate(new_list):
+#         worksheet.write_row(row_num, 0, data)
+
+#     for row_num, data in enumerate(new_list_0):
+#         worksheet.write_row(row_num, 0, data)
 
