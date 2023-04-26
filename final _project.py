8ca2b421453bf5113b92c1a751cc5cc91e7f8b0f
Mehran-Rahimzadeh <mehran1988r@gmail.com>
2023-04-26 17:03:35 +0330

    calculating one person average and writing that in to pre exell sheet

--- final _project.py
+++ final _project.py
@@ -4,18 +4,16 @@ clear=lambda: os.system("clear")
 clear()
 # ---------------------
 
-# importing pyhton list to exell file module
-import xlsxwriter
-
 
 # creat empty list for student names and ther cours 
-a=[]
+data=[]
 student=0
 
 
 #asking funxtion
 def ask():
-    globals()["student"]= dict(firstName="" , lastName = "", courses=[], credits=[], scores=[])
+    globals()["student"]= dict(firstName="" , lastName = "",
+                                courses=[], credits=[], scores=[])
     name_ask()
     courses_ask()
     next_step_student()
@@ -31,7 +29,7 @@ def name_ask():
 def courses_ask():
     student["courses"].append(input ("Enter the student course: "))
     student["credits"].append(int(input ("Enter the course credit: ")))
-    student["scores"].append(float(input ("Enter the course sores: ")))
+    student["scores"].append(float(input ("Enter the course scores: ")))
     next_step_courses()
 
 
@@ -49,123 +47,108 @@ def next_step_courses():
 
     # input method (courses)
 def next_step_student():
+    # student["courses"]=str(student["courses"])
+    # student["credits"]=str(student["credits"])
+    # student["scores"]=str(student["scores"])
+
+    student["courses"]=( ", ".join( repr(e) for e in student["courses"]) )
+    student["credits"]=( ", ".join( repr(e) for e in student["credits"] ) )
+    student["scores"]=( ", ".join( repr(e) for e in student["scores"] ) )
     answer=ord(input('Is there any other students? y/n: '))
     if answer==121:
-        a.append(student)
+        data.append(student)
         ask()
     else:
-        a.append(student)
-        print(*a, sep="\n")
+        data.append(student)
+        print(*data, sep="\n")
 
-# ask()
+ask()
 # make_title_exell()
 
+import openpyxl 
+from openpyxl import Workbook
+from openpyxl import load_workbook
 
-#--------------------------------Writing Data in to an Exell file---------------------------------------------------
-#                ---------------------------------------------------------------------------------
-#                              ----------------------------------
+row = 0
+col = 0
+wb = Workbook()
+ws = wb.active
 
+def create_xls(filepath):
+    wb = Workbook()
+    wb.save(filepath)
+    from openpyxl.styles import Font
+    ft = Font(bold=True)
+    for row in ws["A1:E1"]:
+        for cell in row:
+            cell.font = ft
 
-#Create an new Excel file and add a worksheet.
-workbook=xlsxwriter.Workbook("karnameh_1.xlsx")
-worksheet = workbook.add_worksheet()
+def write_xls(filepath, dictionary):
+    wb = load_workbook(filepath)
+    ws = wb.active
 
-# Widen the first column to make the text clearer.
-worksheet.set_column("A:A", 10)
-worksheet.set_column("B:B", 10)
-worksheet.set_column("C:C", 40)
-worksheet.set_column("D:D", 20)
-worksheet.set_column("E:E", 20)
-
-# Add a bold format to use to highlight cells.
-bold = workbook.add_format({"bold":True})
-
-
-# Write some header.
-worksheet.write('A1', "First Name", bold)
-worksheet.write('B1', 'Last Name', bold)
-worksheet.write('C1', 'Courses', bold)
-worksheet.write('D1', 'Credits', bold)
-worksheet.write('E1', 'Scores', bold)
+    headers = [x for x in dictionary[0]]
+    for index, value in enumerate(headers):
+         ws.cell(row=1, column=index+1).value = value
 
 
-workbook.close()
+    for i, x in enumerate(dictionary):
+        for idx,value in enumerate(x.values()):
+            ws.cell(row=i+2, column=idx+1).value = value
+    wb.save(filepath)
+
+create_xls("example_op.xlsx")
+write_xls("example_op.xlsx", data)
+
+# ----------sheet style-----
+from openpyxl.styles import Font
+ft = Font(bold=True)
+for row in ws["A1:E1"]:
+    for cell in row:
+        cell.font = ft
+
+
+
+
+
+#----------------------------Writing Data in to an Exell file-----
+#                --------------------------------------------------
+#                              -------------------------------
+
+
+# #Create an new Excel file and add a worksheet.
+# workbook=xlsxwriter.Workbook("karnameh_1.xlsx")
+# worksheet = workbook.add_worksheet()
+
+# # Widen the first column to make the text clearer.
+# worksheet.set_column("A:A", 10)
+# worksheet.set_column("B:B", 10)
+# worksheet.set_column("C:C", 40)
+# worksheet.set_column("D:D", 20)
+# worksheet.set_column("E:E", 20)
+
+# # Add a bold format to use to highlight cells.
+# bold = workbook.add_format({"bold":True})
+
+
+# # Write some header.
+# worksheet.write('A1', "First Name", bold)
+# worksheet.write('B1', 'Last Name', bold)
+# worksheet.write('C1', 'Courses', bold)
+# worksheet.write('D1', 'Credits', bold)
+# worksheet.write('E1', 'Scores', bold)
+
+
+# workbook.close()
 
 
 
 # ---------------------------------Inserting Data in Exell file and make version 2-------------------
 
 
-#
-import openpyxl 
-from openpyxl import Workbook
-#wb = Workbook()
+# from openpyxl import Workbook
+# #wb = Workbook()
 
-
-
-# open workbook
-from openpyxl import load_workbook
-wb = load_workbook(filename = 'karnameh_1.xlsx')
-ws = wb.active
-
-ws["A4"]=56
-c= ws.cell(row=12, column=9, value= 0)
-
-# ws1 =wb.create_sheet("Mysheet")
-
-
-#Ranges of cells 
-c.value = 'hello, world'
-
-
-wb.save("karnameh_2.xlsx")
-#print(wb.sheetnames)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-
-
-
-
-
-
-
-
-
-
-
-
-# def make_title_exell():
-#     new_list = [['First Names', 'Last Names', "courses", "credits", "Marks"]]
-#     with xlsxwriter.Workbook('test.xlsx') as workbook:
-#         worksheet = workbook.add_worksheet()
-
-#         for row_num, data in enumerate(new_list):
-#             worksheet.write_row(row_num, 0, data)
-
-#         for row_num, data in enumerate(new_list):
-#             worksheet.write_row(row_num, 0, data)
+# # open workbook
+# from openpyxl import load_workbook
+# wb = load_workbook(filename = 'karnameh_1.xlsx')
